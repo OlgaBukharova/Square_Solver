f789fba369e784c060e4258f838760df5813bc1e
OlgaBukharova <bukharova.on@phystech.edu>
2022-09-07 08:58:29 +0300

    structure added

--- source/Square_Solver.cpp
+++ source/Square_Solver.cpp
@@ -1,84 +1,94 @@
 #include <stdio.h>
 #include <math.h>
 
-void solve_zero_deg(double c, int * num)
+struct equation
 {
-    if (c != 0)
+    double a;
+    double b;
+    double c;
+    int number;
+    double x1;
+    double x2;
+};
+
+void solve_zero_deg(struct equation * ex)
+{
+    if (ex -> c != 0)
     {
-        * num = 0;
+        ex -> number = 0;
     }
 }
 
-void solve_linear(double b, double c, double * x, int * num)
+void solve_linear(struct equation * ex)
 {
-    * num = 1;
-    * x = -c / b;
+    ex -> number = 1;
+    ex -> x1 = -(ex -> c) / (ex -> b);
 }
 
-void solve_quadratic(double a, double b, double c, double * x1, double * x2, int * num)
+void solve_quadratic(struct equation * ex)
 {
-    double discr = b * b - 4 * a * c;
+    double discr = (ex -> b) * (ex -> b) - 4 * (ex -> a) * (ex -> c);
 
     if (discr < 0)
     {
-        * num = 0;
+        ex -> number = 0;
     }
     else
     {
         if (discr == 0)
         {
-            * num = 1;
+            ex -> number = 1;
 
-            * x1 = -b / (2 * a);
+            ex -> x1 = -(ex -> b) / (2 * (ex -> a));
         }
         else 
         {
-            * num = 2;
+            ex -> number = 2;
 
             double sqrt_discr = sqrt(discr);
 
-            * x1 = (-b - sqrt_discr) / (2 * a);
-            * x2 = (-b + sqrt_discr) / (2 * a);
+            ex -> x1 = (-(ex -> b) - sqrt_discr) / (2 * (ex -> a));
+            ex -> x2 = (-(ex -> b) + sqrt_discr) / (2 * (ex -> a));
         }
     }
 }
 
-void input_coef(double * a, double * b, double * c)
+void input_coef(struct equation * ex)
 {
-    scanf("%lf%lf%lf", a, b, c);
+    scanf("%lf%lf%lf", & (ex -> a), & (ex -> b), & (ex -> c));
 }
 
-void solve_equation(double a, double b, double c, double * x1, double * x2, int * num)
+void solve_equation(struct equation * ex)
 {
-    if (a == 0) 
+    if (ex -> a == 0) 
     {
-        if (b == 0) 
+        if (ex -> b == 0) 
         {
-            solve_zero_deg(c, num);
+            solve_zero_deg(ex);
         }
         else 
         {
-            solve_linear(b, c, x1, num);
+            solve_linear(ex);
         }
     }
     else
     {
-        solve_quadratic(a, b, c, x1, x2, num);
+        solve_quadratic(ex);
     }
 }
 
-void output_solution(int num, double x1, double x2)
+void output_solution(struct equation * ex)
 {
-    switch (num)
+    switch (ex -> number)
     {
         case 0:
             printf("No solutions\n");
             break;
         case 1:
-            printf("X = %lg\n", x1);
+            printf("X = %lg\n", ex -> x1);
             break;
         case 2:
-            printf("X1 = %lg\nX2 = %lg\n", x1, x2);
+            printf("X1 = %lg\nX2 = %lg\n", ex -> x1, ex -> x2);
             break;
         default:
             printf("All numbers\n"); 
@@ -87,18 +97,19 @@ void output_solution(int num, double x1, double x2)
 
 int main()
 {
-    double a = NAN;
-    double b = NAN;
-    double c = NAN;
-    double x1 = NAN;
-    double x2 = NAN;
-    int num = -1;
+    struct equation user_eq;
+    user_eq.a = NAN;
+    user_eq.b = NAN;
+    user_eq.c = NAN;
+    user_eq.x1 = NAN;
+    user_eq.x2 = NAN;
+    user_eq.number = -1;
 
-    input_coef(&a, &b, &c);
+    input_coef(&user_eq);
 
-    solve_equation(a, b, c, &x1, &x2, &num);
+    solve_equation(&user_eq);
 
-    output_solution(num, x1, x2);
+    output_solution(&user_eq);
 
     return 0;
-} 
+}
