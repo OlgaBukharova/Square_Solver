635b5d505b8958053edca7f638428cc1a775dabf
OlgaBukharova <bukharova.on@phystech.edu>
2022-10-06 10:37:26 +0300

    Information for user added

--- source/main.cpp
+++ source/main.cpp
@@ -5,9 +5,14 @@ int main()
 {
     struct equation user_eq = {};
 
+    printf("This programm will solve any quadratic equation\n");
+    printf("Input coefficients A, B, C of quadratic equation Ax^2 + Bx + C = 0\n");
+
     input_coef(&user_eq);
 
     solve_equation(&user_eq);
+
+    printf("\nSolutions:\n");
 
     output_solution(&user_eq);
 
